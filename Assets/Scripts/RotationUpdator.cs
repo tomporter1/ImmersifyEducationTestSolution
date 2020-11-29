800febfe6a25457f632d2d10e113ae6dbb51b7da
Tom Porter <tomporter62@gmail.com>
2020-11-29 18:18:02 +0000

    Added UI sliders for scale and rotation

---
+++ Assets/Scripts/RotationUpdator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class RotationUpdator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _model;
+    public void SetModelRotation()
+    {
+        float newRotation = GetComponent<Slider>().value;
+        _model.localEulerAngles = new Vector3(_model.localEulerAngles.x, newRotation, _model.localEulerAngles.z);
+    }
+}
---
+++ Assets/Scripts/ScaleUpdator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class ScaleUpdator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _model;
+    public void SetModelScale()
+    {
+        float newScale = GetComponent<Slider>().value;
+        _model.localScale = new Vector3(newScale, newScale, newScale);
+    }
+}
